83f7bc066f7322ec4043d18d6d39c2a4e8042cdd
darri <darofu04@gmail.com>
2025-02-24 11:41:29 +0100

    Botones bien colocados

--- Scripts/calculator.gd
+++ Scripts/calculator.gd
@@ -10,16 +10,13 @@ var operation: String
 
 
 func _on_uno_pressed() -> void:
-	input += '1'
-	price.text = input
+	selectNumber('1')
 
 func _on_dos_pressed() -> void:
-	input += '2'
-	price.text = input
+	selectNumber('2')
 
 func _on_tres_pressed() -> void:
-	input += '3'
-	price.text = input
+	selectNumber('3')
 
 func _on_suma_pressed() -> void:
 	res = int(input)
@@ -44,58 +41,56 @@ func _on_resultado_pressed() -> void:
 	res = 0
 
 func _on_cuatro_pressed() -> void:
-	input += '4'
-	price.text = input
+	selectNumber('4')
 
 func _on_cinco_pressed() -> void:
-	input += '5'
-	price.text = input
+	selectNumber('5')
 
 func _on_seis_pressed() -> void:
-	input += '6'
-	price.text = input
+	selectNumber('6')
 
 func _on_multiplicacion_pressed() -> void:
-	res = int(input)
+	res = float(input)
 	input = ''
 	operation = '*'
 	price.text = input
 
 func _on_siete_pressed() -> void:
-	input += '7'
-	price.text = input
+	selectNumber('7')
 
 
 func _on_ocho_pressed() -> void:
-	input += '8'
-	price.text = input
+	selectNumber('8')
 
 func _on_nueve_pressed() -> void:
 	input += '9'
 	price.text = input
 
 func _on_resta_pressed() -> void:
-	res = int(input)
+	res = float(input)
 	input = ''
 	operation = '-'
 	price.text = input
 
 func _on_modulo_pressed() -> void:
-	res = int(input)
+	res = float(input)
 	input = ''
 	operation = '%'
 	price.text = input
 
 func _on_cero_pressed() -> void:
-	input += '0'
-	price.text = input
+	selectNumber('0')
 
 func _on_division_pressed() -> void:
-	res = int(input)
+	res = float(input)
 	input = ''
 	operation = '/'
 	price.text = input
 
 func _on_decimal_pressed() -> void:
 	input += '.'
+	price.text = input
+
+func selectNumber(number):
+	input += number
 	price.text = input

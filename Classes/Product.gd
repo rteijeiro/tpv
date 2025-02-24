86d8e57854c3546e731af7e58525fb6e39b0a117
Tonii18 <ajsanrosal@gmail.com>
2025-02-24 13:26:13 +0100

    Class product and JSON file created with some examples

---
+++ Classes/Product.gd
@@ -0,0 +1,11 @@
+class_name Product
+
+var type: String
+var name: String
+var price: float
+
+# Constructor
+func _init(type, name, price):
+	self.type = type
+	self.name = name
+	self.price = price

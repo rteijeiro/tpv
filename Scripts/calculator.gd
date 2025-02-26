51e93c7d8e6ad1b527428601b38d82e3c54d2343
darri <darofu04@gmail.com>
2025-02-26 09:07:55 +0100

    Calculadora restructurada

--- Scripts/calculator.gd
+++ Scripts/calculator.gd
@@ -9,58 +9,53 @@ var operation: String
 @onready var price = $PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/Price
 
 
-func _on_uno_pressed() -> void:
+func _on_one_pressed() -> void:
 	selectNumber('1')
 
-func _on_dos_pressed() -> void:
+func _on_two_pressed() -> void:
 	selectNumber('2')
 
-func _on_tres_pressed() -> void:
+func _on_three_pressed() -> void:
 	selectNumber('3')
 
-func _on_cuatro_pressed() -> void:
+func _on_sum_pressed() -> void:
+	selectOperation('+')
+
+func _on_four_pressed() -> void:
 	selectNumber('4')
 
-func _on_cinco_pressed() -> void:
+func _on_five_pressed() -> void:
 	selectNumber('5')
 
-func _on_seis_pressed() -> void:
+func _on_six_pressed() -> void:
 	selectNumber('6')
 
-func _on_siete_pressed() -> void:
+func _on_multiplication_pressed() -> void:
+	selectOperation('*')
+
+func _on_seven_pressed() -> void:
 	selectNumber('7')
 
-func _on_ocho_pressed() -> void:
+func _on_eight_pressed() -> void:
 	selectNumber('8')
 
-func _on_nueve_pressed() -> void:
+func _on_nine_pressed() -> void:
 	selectNumber('9')
 
-func _on_cero_pressed() -> void:
+func _on_subtraction_pressed() -> void:
+	selectOperation('-')
+
+func _on_percent_pressed() -> void:
+	selectOperation('%')
+
+func _on_zero_pressed() -> void:
 	selectNumber('0')
 
 func _on_decimal_pressed() -> void:
 	selectNumber('.')
 
-func _on_suma_pressed() -> void:
-	selectOperation('+')
-
-func _on_resta_pressed() -> void:
-	selectOperation('-')
-
-func _on_multiplicacion_pressed() -> void:
-	selectOperation('*')
-
-func _on_division_pressed() -> void:
+func _on_slash_pressed() -> void:
 	selectOperation('/')
- 
-func _on_modulo_pressed() -> void:
-	selectOperation('%')
-
-func _on_delete_pressed() -> void:
-	if input.length() > 0:
-		input[-1] = ''
-		price.text = input
 
 func selectNumber(number):
 	input += number
@@ -82,7 +77,7 @@ func selectOperation(element):
 	input = ''
 	price.text = input
 
-func _on_resultado_pressed() -> void:
+func _on_equals_pressed() -> void:
 	if operation == '+':
 		res += float(input)
 	if operation == '-':
@@ -96,3 +91,8 @@ func _on_resultado_pressed() -> void:
 	price.text = str(res)
 	input = ''
 	res = 0
+
+func _on_delete_pressed() -> void:
+	if input.length() > 0:
+		input[-1] = ''
+		price.text = input
